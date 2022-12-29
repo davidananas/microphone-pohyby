72d240ecbcbd1ffeaf6aaf40c2367c7d94083964
davidananas <116786922+davidananas@users.noreply.github.com>
2022-12-29 14:16:54 +0100

    Aktualizovat main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,17 +1,33 @@
 input.onButtonPressed(Button.A, function () {
     strip.showColor(neopixel.colors(NeoPixelColors.Yellow))
+    basic.showArrow(ArrowNames.South)
     kitronik_servo_lite.backward()
 })
 input.onSound(DetectedSound.Loud, function () {
     strip.showColor(neopixel.colors(NeoPixelColors.Green))
+    basic.showArrow(ArrowNames.North)
     kitronik_servo_lite.forward()
 })
 input.onSound(DetectedSound.Quiet, function () {
     strip.showColor(neopixel.colors(NeoPixelColors.Red))
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . # . .
+        . . . . .
+        . . . . .
+        `)
     kitronik_servo_lite.stop()
 })
 input.onLogoEvent(TouchButtonEvent.Pressed, function () {
     strip.showColor(neopixel.colors(NeoPixelColors.Red))
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . # . .
+        . . . . .
+        . . . . .
+        `)
     kitronik_servo_lite.stop()
 })
 let strip: neopixel.Strip = null
